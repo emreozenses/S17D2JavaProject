3cccaef962d55586779ee94eee6112147878452b
Emre OZENSES <emreozenses@gmail.com>
2023-12-27 19:40:30 +0300

    Initial commit

---
+++ src/main/java/com/workintech/S17D2JavaProject/S17D2JavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.S17D2JavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S17D2JavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S17D2JavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/S17D2JavaProject/S17D2JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.S17D2JavaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S17D2JavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
